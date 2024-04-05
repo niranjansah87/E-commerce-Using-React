d10a394d08e64b319c691ddae134de5f3ad92949
Niranjan <2100090187@kluniversity.in>
2024-04-05 17:12:17 +0530

    made database connection

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const mongoURI="mongodb://localhost:27017/E_Commerce";
+
+const connectToMongoose = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (err) {
+
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+module.exports = connectToMongoose;
